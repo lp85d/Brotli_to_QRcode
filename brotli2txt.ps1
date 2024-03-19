d3ab8a62c0fe811220e6ca8ea6f9cc8b8a72d255
Denis Sochi <lp85d@mail.ru>
2024-03-20 01:02:51 +0300

    Create brotli2txt.ps1
    GPU.ps1 это код программы который я хотел сжать алгоритмом Brotli и записать в файл brotli.txt

---
+++ brotli2txt.ps1
@@ -0,0 +1,11 @@
+# Путь к brotli.exe
+$brotliPath = "C:\brotli\brotli.exe"
+
+# Путь к файлу для сжатия
+$filePath = "C:\Users\user\Desktop\GPU.ps1"
+
+# Путь к файлу, в который будет записан сжатый результат
+$outputPath = "C:\Users\user\Desktop\brotli.txt"
+
+# Вызов brotli.exe для сжатия файла
+& $brotliPath $filePath -o $outputPath
