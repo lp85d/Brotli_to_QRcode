c0decd98577c23d8ebaa9f3932f945cafcb93584
Denis Sochi <lp85d@mail.ru>
2024-04-22 01:21:44 +0300

    Update brotli2txt.ps1

--- brotli2txt.ps1
+++ brotli2txt.ps1
@@ -2,7 +2,7 @@
 $brotliPath = "C:\brotli\brotli.exe"
 
 # Путь к файлу для сжатия
-$filePath = "C:\Users\user\Desktop\GPU.ps1"
+$filePath = "C:\Users\user\Desktop\ANSI_1.ps1"
 
 # Путь к файлу, в который будет записан сжатый результат
 $outputPath = "C:\Users\user\Desktop\brotli.txt"
