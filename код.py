28c59f48d0d2b50c1954d982fba07b3184598527
Denis Sochi <lp85d@mail.ru>
2025-08-05 10:26:24 +0400

    Update код.py

--- код.py
+++ код.py
@@ -1,3 +1,5 @@
+//C:\brotli\brotli.exe -Z C:\vosk\voice_control.py -o C:\vosk\voice_control.py.br
+
 import segno
 
 with open(r'C:\vosk\voice_control.py.br', 'rb') as f:
@@ -6,3 +8,4 @@ with open(r'C:\vosk\voice_control.py.br', 'rb') as f:
 # Генерируем QR-код напрямую из байтов (без Base64)
 qr = segno.make(data, error='L')  # минимальный уровень коррекции
 qr.save(r'C:\vosk\qr_raw_binary.png', scale=5)
+
